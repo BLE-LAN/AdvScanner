f1204350da7d7cb9f3a3ebc7d0e7eb4d559787a4
nyaboron <nyaboronnn@protonmail.com>
2021-07-22 12:44:22 +0200

    Parse data type 0x09 (complete localname), chage dBm to RSSI.

--- GetBeacons/Parser.cpp
+++ GetBeacons/Parser.cpp
@@ -16,6 +16,8 @@
 #include <iostream>
 
 #include <wrl/client.h>
+#include <wrl/wrappers/corewrappers.h>
+#include <hstring.h>
 
 #include "Parser.h"
 #include "../rapidjson/include/rapidjson/document.h"
@@ -23,12 +25,21 @@
 #include "../rapidjson/include/rapidjson/stringbuffer.h"
 
 
+
 using namespace std;
 using namespace Microsoft::WRL;
 using namespace rapidjson;
+using namespace Microsoft::WRL::Wrappers;
 
-#define BD_ADDR_LEN 6
 
+char _bluetoothLEAdvertisementStrings[6][27] = {
+    "ConnectableUndirected",
+    "ConnectableDirected",
+    "ScannableUndirected",
+    "NonConnectable",
+    "ScanResponse",
+    "Extended"
+};
 
 void addressGetBytesInBigEndian(UINT8 bda[], UINT64* bluetoothAddress)
 {
@@ -63,7 +74,7 @@ void adtypeParse(IBluetoothLEAdvertisementReceivedEventArgs* args, Document& doc
     args->get_AdvertisementType(&advtype);
 
     Value v_advtype;
-    v_advtype.SetInt(advtype);
+    v_advtype.SetString(_bluetoothLEAdvertisementStrings[advtype], strlen(_bluetoothLEAdvertisementStrings[advtype]), document.GetAllocator());
     document.AddMember("advtype", v_advtype, document.GetAllocator());
 }
 
@@ -73,7 +84,7 @@ void rawsignalParse(IBluetoothLEAdvertisementReceivedEventArgs* args, Document& 
 
     Value v_dbm;
     v_dbm.SetInt(dbm);
-    document.AddMember("dbm", v_dbm, document.GetAllocator());
+    document.AddMember("RSSI", v_dbm, document.GetAllocator());
 }
 
 void timestampParse(IBluetoothLEAdvertisementReceivedEventArgs* args, Document& document) 
@@ -83,8 +94,27 @@ void timestampParse(IBluetoothLEAdvertisementReceivedEventArgs* args, Document& 
     ctime_s(dateBuff, sizeof dateBuff, &result);
 
     Value v_timestamp;
-    v_timestamp.SetString(dateBuff, sizeof(dateBuff), document.GetAllocator());
+    // ctime_s add a '\n' at the of the string, thats the -1
+    v_timestamp.SetString(dateBuff, strlen(dateBuff)-1, document.GetAllocator());
     document.AddMember("timestamp", v_timestamp, document.GetAllocator());
+}
+
+
+void localNameParse(ComPtr<IBluetoothLEAdvertisement> bleAdvert, Document& document)
+{
+    // Get Name of the device
+    HString name;
+    char buff_name[128] = { 0 };
+    
+    bleAdvert->get_LocalName(name.GetAddressOf());
+   
+    if (wcslen(name.GetRawBuffer(nullptr)) != 0) {
+        sprintf_s(buff_name, sizeof(buff_name), "%S", name.GetRawBuffer(nullptr));
+
+        Value v_localname;
+        v_localname.SetString(buff_name, strlen(buff_name), document.GetAllocator());
+        document.AddMember("completename", v_localname, document.GetAllocator());
+    }
 }
 
 bool Parser::Parser(IBluetoothLEAdvertisementReceivedEventArgs* args) 
@@ -106,15 +136,21 @@ bool Parser::Parser(IBluetoothLEAdvertisementReceivedEventArgs* args)
     
     /*
         Data Section
-        los dataTypes no 'conocidos' tendrá como nombre el valor numérico del dataType y valor los bytes
     */
 
-    /*
-     ComPtr<IBluetoothLEAdvertisement> bleAdvert;
+    // get_Flags 0x01
+    // get_ServiceUuids 0x03
+    // get_ManufacturerData 0xff
+
+    ComPtr<IBluetoothLEAdvertisement> bleAdvert;
     args->get_Advertisement(&bleAdvert);
 
+    localNameParse(bleAdvert, document);
+
+    HRESULT hr;
+
     ComPtr <ABI::Windows::Foundation::Collections::IVector<ABI::Windows::Devices::Bluetooth::Advertisement::BluetoothLEAdvertisementDataSection*>> vecData;
-    HRESULT hr = bleAdvert->get_DataSections(&vecData);
+     hr = bleAdvert->get_DataSections(&vecData);
 
     char auxBuff[256] = { 0 };
     unsigned int countDataSections = 0;
@@ -130,18 +166,20 @@ bool Parser::Parser(IBluetoothLEAdvertisementReceivedEventArgs* args)
         BYTE datatype = 0;
         hr = significantPart->get_DataType(&datatype);
 
+        // 
+
         memset(auxBuff, 0, sizeof(auxBuff));
         sprintf_s(auxBuff, sizeof(auxBuff), "%X", datatype);
         printf("Data Type: %s ", auxBuff);
 
-        hr = significantPart->get_Data(&ibuf);
-    }*/
+       // hr = significantPart->get_Data(&ibuf);
+    }
 
 
     StringBuffer buffer;
     Writer<StringBuffer> writer(buffer);
     document.Accept(writer);
-    std::cout << buffer.GetString() << std::endl;
+    std::cout << "\n" << buffer.GetString() << std::endl;
 
     return true;
 }
--- GetBeacons/Parser.h
+++ GetBeacons/Parser.h
@@ -3,9 +3,10 @@
 #include <../winrt/windows.devices.bluetooth.h>
 using namespace ABI::Windows::Devices::Bluetooth::Advertisement;
 
+// Six oct for the ble address xx:xx:xx:xx:xx:xx
+#define BD_ADDR_LEN 6
+
 namespace Parser 
 {
 	bool Parser(IBluetoothLEAdvertisementReceivedEventArgs* args);
-	void BluetoothAddressToString(UINT8 bda[], UINT64* btha);
-	unsigned int AdvertisementTypeToString(BluetoothLEAdvertisementType type, char* name);
 }
